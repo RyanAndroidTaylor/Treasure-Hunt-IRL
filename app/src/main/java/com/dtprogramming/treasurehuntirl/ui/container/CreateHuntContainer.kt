5f78208c59e0a44005f453e46832127b6ce69bd2
Ryan Taylor <rtaylor@verisage.us>
2016-06-22 21:46:33 -0600

    Added views for adding way points, saving and canceling the create hunt screen

--- app/src/main/java/com/dtprogramming/treasurehuntirl/ui/container/CreateHuntContainer.kt
+++ app/src/main/java/com/dtprogramming/treasurehuntirl/ui/container/CreateHuntContainer.kt
@@ -1,6 +1,7 @@
 package com.dtprogramming.treasurehuntirl.ui.container
 
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.dtprogramming.treasurehuntirl.R
 import com.dtprogramming.treasurehuntirl.presenters.CreateHuntPresenter
@@ -16,25 +17,32 @@ class CreateHuntContainer(val createHuntPresenter: CreateHuntPresenter, val clue
 
     private lateinit var adapter: ClueAdapter
 
+    private lateinit var clueList: RecyclerView
+
     override fun inflate(parent: ViewGroup): Container {
         return super.inflate(parent, R.layout.container_create_hunt)
     }
 
     override fun loadViews(parent: ViewGroup) {
-        parent.create_hunt_container_clue_list.layoutManager = CustomLinearLayoutManager(parent.context, LinearLayoutManager.HORIZONTAL, false)
+        clueList = parent.create_hunt_container_clue_list
 
-//        for (i in 0..100)
-//            clues.add("This is a clue to a sweet treasure hunt that only a few people will ever figure out. That is how good it is. We need a lot more text in here so it looks good to the eye when we are playing with in on the screen")
+        clueList.layoutManager = CustomLinearLayoutManager(parent.context, LinearLayoutManager.HORIZONTAL, false)
 
         adapter = ClueAdapter(parent.context, clues)
 
-        parent.create_hunt_container_clue_list.adapter = adapter
+        clueList.adapter = adapter
 
-        parent.create_hunt_add_clue.setOnClickListener {
+        parent.create_hunt_container_add_clue.setOnClickListener {
             createHuntPresenter.switchState(CreateHuntPresenter.CREATE_CLUE)
         }
 
-        parent.create_hunt_container_clue_list.addOnScrollListener(ClueScrollListener())
+        parent.create_hunt_container_add_waypoint.setOnClickListener {  }
+
+        parent.create_hunt_container_save.setOnClickListener {  }
+
+        parent.create_hunt_container_cancel.setOnClickListener {  }
+
+        clueList.addOnScrollListener(ClueScrollListener())
     }
 
     fun updateClueList(clues: List<String>) {
